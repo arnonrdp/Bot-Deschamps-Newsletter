350bc920518946f5a2bc2cd00f5214079c5daa3d
Arnon Rodrigues <arnonrdp@gmail.com>
2021-12-29 16:30:00 -0300

    Refatorado **post_tweet.py**
    - A função **prepare_tweet** foi subdividida em duas funções;
    - Foram adicionados *try/except* em duas funções.

--- post_tweet.py
+++ post_tweet.py
@@ -4,15 +4,23 @@ from time import sleep
 from tweepy import OAuthHandler, API
 
 
-def prepare_tweet(tweet_list, api):
+def twitter_connect(tweet_list):
     auth = OAuthHandler(getenv('API_KEY'),
                         getenv('API_KEY_SECRET'))
     auth.set_access_token(getenv('ACCESS_TOKEN'),
                           getenv('ACCESS_TOKEN_SECRET'))
     api = API(auth)
-    api.verify_credentials()
-    print('Twitter: conexão bem-sucedida!')
-    prepare_tweet(tweet_list, api)
+    try:
+        api.verify_credentials()
+        print('Twitter: conexão bem-sucedida!')
+        prepare_tweet(tweet_list, api)
+    except BaseException as e:
+        print('Twitter: conexão mal-sucedida! Tentando novamente em 2 minutos.\n', e)
+        sleep(120)
+        twitter_connect(tweet_list)
+
+
+def prepare_tweet(tweet_list, api):
     for tweet in tweet_list:
         tweet_encoded = tweet.encode("utf-8", "ignore")
         tweet_decoded = tweet_encoded.decode()
@@ -24,13 +32,17 @@ def prepare_tweet(tweet_list, api):
 def post_tweet(api, each_tweet):
     original_tweet = []
     for i, chunk in zip(range(len(each_tweet)), each_tweet):
-        original_tweet.extend([chunk])
-        print([chunk])
-        if i == 0:
-            original_tweet[i] = api.update_status(chunk)
-        else:
-            original_tweet[i] = api.update_status(chunk,
-                                                    in_reply_to_status_id=original_tweet[i - 1].id,
-                                                    auto_populate_reply_metadata=True)
-        sleep(1)
+        try:
+            original_tweet.extend([chunk])
+            print([chunk])
+            if i == 0:
+                original_tweet[i] = api.update_status(chunk)
+            else:
+                original_tweet[i] = api.update_status(chunk,
+                                                      in_reply_to_status_id=original_tweet[i - 1].id,
+                                                      auto_populate_reply_metadata=True)
+            sleep(1)
+        except Exception as e:
+            print('Erro: ', e)
+            break
     sleep(300)
