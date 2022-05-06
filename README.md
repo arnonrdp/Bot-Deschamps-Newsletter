3352e0ef48f978d155ee9d067f11852a5c044e5f
Arnon <arnonrdp@gmail.com>
2022-05-06 16:51:35 -0300

    Funções e chamadas simplificadas

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
-<p align="center">
-  <a href="https://github.com/arnonrdp/Bot-Deschamps-Newsletter/actions/workflows/main.yml">
-    <img src="https://github.com/arnonrdp/Bot-Deschamps-Newsletter/actions/workflows/main.yml/badge.svg" alt="Robot with stylish hair"/>
-  </a>
-  <img alt="GitHub code size in bytes" src="https://img.shields.io/github/languages/code-size/arnonrdp/Bot-Deschamps-Newsletter">
-  <a href="https://twitter.com/BotDeschamps">
-    <img src="https://img.shields.io/twitter/follow/BotDeschamps?style=social" alt="Twitter Follow"/>
-  </a>
-</p>
-<br>
-<p align="center">
-  <a href="https://twitter.com/BotDeschamps">
-    <img src="./img/avatar.png" alt="Avatar"/>
-  </a>
-</p>
+<div style="text-align: center;">
+
+[![Python Script Workflow](https://github.com/arnonrdp/Bot-Deschamps-Newsletter/actions/workflows/main.yml/badge.svg)](https://github.com/arnonrdp/Bot-Deschamps-Newsletter/actions/workflows/main.yml)
+![Code Size](https://img.shields.io/github/languages/code-size/arnonrdp/Bot-Deschamps-Newsletter)
+[![Twitter Follow Button](https://img.shields.io/twitter/follow/BotDeschamps?style=social)](https://twitter.com/BotDeschamps)
+
+[![BotDeschamps](./img/avatar.png)](https://twitter.com/BotDeschamps)
+
+</div>
--- read_email.py
+++ read_email.py
@@ -1,27 +1,20 @@
-import logging
 from os import getenv
 from time import sleep
-from imap_tools import MailBox, MailMessageFlags, A
+from imap_tools import A, MailBox, MailMessageFlags
 from post_tweet import twitter_connect
+
+attempts = 0
 
 
 def mail_connect():
-    mailbox = MailBox('imap.gmail.com').login(getenv('MAIL'),
-                                              getenv('PASS'),
-                                              initial_folder='INBOX')
-    print('\nPrint – Gmail: conexão bem-sucedida!')
-    logging.debug("Hello World")
-    logger1 = logging.getLogger("module_1")
-    logger2 = logging.getLogger("module_2")
-
-    logger1.debug("Module 1 debugger")
-    logger2.debug("Module 2 debugger")
+    mailbox = MailBox('imap.gmail.com').login(getenv('MAIL'), getenv('PASS'), initial_folder='INBOX')
+    print('\nGmail: conexão bem-sucedida!')
     read_email(mailbox)
 
 
 def read_email(mailbox):
     mail_list = [i.uid for i in mailbox.fetch()]
-    prepare_mail(mailbox) if mail_list else countdown(900)
+    prepare_mail(mailbox) if mail_list else trials()
 
 
 def prepare_mail(mailbox):
@@ -29,30 +22,28 @@ def prepare_mail(mailbox):
     for msg in mailbox.fetch(A(from_='newsletter@filipedeschamps.com.br')):
         posts = msg.text.replace('*', '').split('\r\n\r\n')
         posts = posts[2:-3]
-        mark_as_read(mailbox)
+        mark_as_read(mailbox, msg.uid)
         archive_message(mailbox, msg.uid)
     twitter_connect(posts)
 
 
-def mark_as_read(mailbox):
-    flags = (MailMessageFlags.ANSWERED, MailMessageFlags.FLAGGED)
-    mailbox.flag(mailbox.fetch(A(seen=False)), flags, True)
-    mailbox.flag(mailbox.fetch("SENTON 01-Jan-2021"), MailMessageFlags.SEEN, False)
+def mark_as_read(mailbox, msg_uid):
+    mailbox.flag(msg_uid, MailMessageFlags.SEEN, True)
     print('E-mail marcado como lido.')
 
 
 def archive_message(mailbox, msg_uid):
-    move_to = 'Tweeted'
-    mailbox.move(msg_uid, move_to)
+    mailbox.move(msg_uid, 'Tweeted')
     print('E-mail arquivado.')
 
 
-def countdown(t):
-    print('Nenhum e-mail encontrado. Tentando novamente em 15 minutos.')
-    while t:
-        minutes, seconds = divmod(t, 60)
-        time_format = '{:02d}:{:02d}'.format(minutes, seconds)
-        print('\r', time_format, end='')
-        sleep(1)
-        t -= 1
+def trials():
+    global attempts
+    attempts += 1
+    print('\nNenhum e-mail encontrado.', end=' ')
+    if attempts == 3:
+        print('Tentativas excedidas.')
+        return
+    print('Tentando novamente em 15 minutos...')
+    sleep(900)
     mail_connect()
