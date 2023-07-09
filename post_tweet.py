79bd7add49a5f7d2667d1cb2de19f328280eeb72
Arnon <arnonrdp@gmail.com>
2023-07-08 23:26:28 -0300

    Updated API methods

--- post_tweet.py
+++ post_tweet.py
@@ -1,46 +1,41 @@
 from os import getenv
 from textwrap import wrap
 from time import sleep
-from tweepy import OAuthHandler, API
+
+from tweepy import Client
 
 
 def twitter_connect(tweet_list):
-    auth = OAuthHandler(getenv('API_KEY'),
-                        getenv('API_KEY_SECRET'))
-    auth.set_access_token(getenv('ACCESS_TOKEN'),
-                          getenv('ACCESS_TOKEN_SECRET'))
-    api = API(auth)
-    try:
-        api.verify_credentials()
-        print('Twitter: conexão bem-sucedida!')
-        prepare_tweet(tweet_list, api)
-    except Exception as e:
-        print('Twitter: conexão mal-sucedida! Tentando novamente em 2 minutos.\n', e)
-        sleep(120)
-        twitter_connect(tweet_list)
+    client = Client(
+        consumer_key=getenv('API_KEY'),
+        consumer_secret=getenv('API_KEY_SECRET'),
+        access_token=getenv('ACCESS_TOKEN'),
+        access_token_secret=getenv('ACCESS_TOKEN_SECRET')
+    )
+
+    prepare_tweet(tweet_list, client)
 
 
-def prepare_tweet(tweet_list, api):
+def prepare_tweet(tweet_list, client):
     for tweet in tweet_list:
         tweet_encoded = tweet.encode("utf-8", "ignore")
         tweet_decoded = tweet_encoded.decode()
         each_tweet = wrap(tweet_decoded, 280, break_long_words=False)
-        post_tweet(api, each_tweet)
+        post_tweet(client, each_tweet)
     print('FIM')
 
 
-def post_tweet(api, each_tweet):
+def post_tweet(client, each_tweet):
     original_tweet = []
     for i, chunk in zip(range(len(each_tweet)), each_tweet):
         try:
             original_tweet.extend([chunk])
             print([chunk])
             if i == 0:
-                original_tweet[i] = api.update_status(chunk)
+                original_tweet[i] = client.create_tweet(text=chunk)
             else:
-                original_tweet[i] = api.update_status(chunk,
-                                                      in_reply_to_status_id=original_tweet[i - 1].id,
-                                                      auto_populate_reply_metadata=True)
+                original_tweet[i] = client.create_tweet(text=chunk,
+                                                        in_reply_to_tweet_id=original_tweet[i - 1].data['id'])
             sleep(1)
         except Exception as e:
             print('Erro: ', e)
